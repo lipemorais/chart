73a81d7e325441d246b2dd017694e6b50329db06
Felipe de Morais <felipejpa15@gmail.com>
2014-11-20 14:34:21 -0200

    fixing the django path to the static folder

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn chart_challenge.wsgi
+web: gunicorn chart_challenge.wsgi  --log-file -
--- chart_challenge/settings.py
+++ chart_challenge/settings.py
@@ -79,7 +79,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'chart_challenge/static'),
 )
 
 # Simplified static file serving.
