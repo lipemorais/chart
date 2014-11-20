0c9001fa651759ae39bae0c0ff7523c5b63b686a
Felipe de Morais <felipejpa15@gmail.com>
2014-11-20 12:22:25 -0200

    insert the logic fo view give the necessary information for the template from the API

--- chart_challenge/views.py
+++ chart_challenge/views.py
@@ -1,8 +1,37 @@
+# -*- coding: utf-8 -*-
+
 from django.views.generic import TemplateView
+from datetime import date
+import requests
 
 
 class ChartView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self):
-        pass
+        context = {}
+        path = 'http://jsonrates.com/historical/?'
+        rates_list = []
+        days_list = '['
+
+        currency = self.request.GET.get('currency', 'USD')
+        currency = currency.upper()
+        from_currency = 'from=' + currency
+        to_currency = 'to=BRL'
+
+        today = date.today()
+        one_week_ago = date(today.year, today.month, today.day - 7)
+        date_end = 'dateEnd=' + today.strftime('%Y-%m-%d')
+        date_start = 'dateStart=' + one_week_ago.strftime('%Y-%m-%d')
+
+        url = path + from_currency + '&' + to_currency + '&' + date_start + '&' + date_end
+        response = requests.get(url)
+        context = response.json()
+        for key, value in context['rates'].iteritems():
+            days_list += '"' + key + '",'
+            rates_list.append(value['rate'])
+        days_list += ']'
+
+        context['days_list'] = days_list
+        context['rates_list'] = rates_list
+        return context
