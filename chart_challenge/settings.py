30d71117bf4a6ee8b65757e0a4508392961d0cc8
Felipe de Morais <felipejpa15@gmail.com>
2014-11-20 14:10:15 -0200

    adding codiguration for serving staticfiles as whitenoise and others things

--- chart_challenge/settings.py
+++ chart_challenge/settings.py
@@ -75,5 +75,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'chart_challenge/static/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
