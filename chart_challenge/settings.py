539ac73481a34f04b74d4e9ec999f2bcc7174d31
Felipe de Morais <felipejpa15@gmail.com>
2014-11-20 14:21:33 -0200

    trying to use dj-static as a static server

--- chart_challenge/settings.py
+++ chart_challenge/settings.py
@@ -75,7 +75,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'chart_challenge/static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'chart_challenge/static/')
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
